0a59d811a52136ffdcb2c9203f75113705174e9e
dmitriy-deomin <58627@bk.ru>
2017-10-13 21:53:21 +0300

    pravka3

--- app/src/main/java/dmitriy/deomin/myapplication/Label.kt
+++ app/src/main/java/dmitriy/deomin/myapplication/Label.kt
@@ -3,15 +3,22 @@ package dmitriy.deomin.myapplication
 
 import android.content.Context
 import android.graphics.Canvas
+import android.util.AttributeSet
 import android.widget.TextView
 import dmitriy.deomin.myapplication.MainActivity.Companion.face
 
 /**
  * Created by dimon on 11.09.2017.
  */
-class Label(context: Context?) : TextView(context) {
+class Label : TextView {
+
+    //конструкторы нужны обязаловкой иначе не фурычит
+    constructor(ctx: Context) : super(ctx) {}
+    constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs) {}
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         this.typeface = face
     }
+
 }
